9e80a8670f3ebb3f6447f5b7d9d915e3b3cb6393
Barış Pişmişoğlu <barispo@gmail.com>
2021-02-14 00:28:39 +0300

    14.02.2020 tarihli dosyalar ın güncellemeleri.

--- OOP3/BasvuruManager.cs
+++ OOP3/BasvuruManager.cs
@@ -8,9 +8,14 @@ namespace OOP3
     {
         public void BasvuruYap(IKrediManager krediManager)  //Başvuran bilgilerini değerlendirme
         {
-                                  
             krediManager.Hesapla();      
-
+        }
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
         }
     }
 }
